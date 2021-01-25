f0f7dac2d3a07bd71c5f448a61e66bde8c58c62b
vistritpandey <vistritpandey@gmail.com>
2021-01-25 15:07:09 +0530

    images

---
+++ constants/images.js
@@ -0,0 +1,17 @@
+export const beach = require("../assets/images/beach.jpeg");
+export const climbingHills = require("../assets/images/climbing_hills.jpeg");
+export const frozenHills = require("../assets/images/frozen_hills.jpeg");
+export const homeBanner = require("../assets/images/home_banner.jpeg");
+export const onboardingImage = require("../assets/images/onboarding_image.jpeg");
+export const skiVilla = require("../assets/images/ski_villa.jpeg");
+export const skiVillaBanner = require("../assets/images/ski_villa_banner.jpeg");
+
+export default {
+  beach,
+  climbingHills,
+  frozenHills,
+  homeBanner,
+  onboardingImage,
+  skiVilla,
+  skiVillaBanner,
+};
