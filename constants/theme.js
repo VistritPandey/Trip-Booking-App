e338d9859c744f5159af03b58bd3e309b464252e
Vistrit <56611775+VistritPandey@users.noreply.github.com>
2021-01-27 11:03:43 +0530

    Bug Fixed

--- constants/theme.js
+++ constants/theme.js
@@ -3,14 +3,6 @@ import { AppLoading } from "expo";
 import { Dimensions } from "react-native";
 const { width, height } = Dimensions.get("window");
 
-/*const fetchFonts = () => {
-    return Font.loadAsync({
-    'roboto-bold': require('./assets/fonts/Roboto-Bold.ttf'),
-    'roboto-italic': require('./assets/fonts/Roboto-Italic.ttf'),
-    'roboto-regular': require('./assets/fonts/Roboto-Regular.ttf')
-    });
-    };*/
-
 export const COLORS = {
   primary: "#5390ff",
   secondary: "#cacfd9",
@@ -40,31 +32,26 @@ export const SIZES = {
 };
 export const FONTS = {
   largeTitle: {
-    fontFamily: "Roboto-Black",
     fontSize: SIZES.largeTitle,
     lineHeight: 55,
   },
-  h1: { fontFamily: "Roboto-Black", fontSize: SIZES.h1, lineHeight: 36 },
-  h2: { fontFamily: "Roboto-Bold", fontSize: SIZES.h2, lineHeight: 30 },
-  h3: { fontFamily: "Roboto-Bold", fontSize: SIZES.h3, lineHeight: 22 },
-  h4: { fontFamily: "Roboto-Bold", fontSize: SIZES.h4, lineHeight: 22 },
+  h1: {  fontSize: SIZES.h1, lineHeight: 36 },
+  h2: {  fontSize: SIZES.h2, lineHeight: 30 },
+  h3: {  fontSize: SIZES.h3, lineHeight: 22 },
+  h4: {  fontSize: SIZES.h4, lineHeight: 22 },
   body1: {
-    fontFamily: "Roboto-Regular",
     fontSize: SIZES.body1,
     lineHeight: 36,
   },
   body2: {
-    fontFamily: "Roboto-Regular",
     fontSize: SIZES.body2,
     lineHeight: 30,
   },
   body3: {
-    fontFamily: "Roboto-Regular",
     fontSize: SIZES.body3,
     lineHeight: 22,
   },
   body4: {
-    fontFamily: "Roboto-Regular",
     fontSize: SIZES.body4,
     lineHeight: 22,
   },
--- screens/Onboarding.js
+++ screens/Onboarding.js
@@ -1,13 +1,46 @@
 import React from "react";
-import { StyleSheet, SafeAreaView, Text, View, Image, TouchableOpacity } from "react-native";
-import LinearGradient from 'react-native-linear-gradient'
+import { StyleSheet, SafeAreaView, Text, View, Image, TouchableOpacity, Button } from "react-native";
+import LinearGradient from 'react-native-linear-gradient';
+import { COLORS, images, SIZES, FONTS } from "../constants"
 
 const Onboarding = () => {
   return (
-    <SafeAreaView>
-      <Text>Onboarding</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={{ flex:1, alignItems: 'center', justifyContent:'center'}} >
+        <Image
+        source={images.onboardingImage}
+        resizeMode="contain"
+        style={{
+          width: '100%',
+          height: '100%'
+        }}
+        />
+      </View>
+      <View style={{ flex:1, alignItems: 'center', justifyContent:'center'}} >
+        <View style={{alignItems: 'center', marginHorizontal: SIZES.padding}} >
+            <Text style={{...FONTS.h2}}>Digital Ticket</Text>
+            <Text style={{color: COLORS.gray, marginTop: SIZES.padding, textAlign: 'center', ...FONTS.body3}}>Book your tickets,hotels and motels easily with the help of this app</Text>
+        </View>
+        <TouchableOpacity
+          style={{marginTop: SIZES.padding*2, width: '70%', height: 50, alignItems: 'center', justifyContent: 'center'}}
+          onPress={() => console.log("Button pressed")}
+        >
+          <Button
+          style={{ height: '100%', width: '100%', alignItems: 'center', justifyContent: 'center', borderRadius: 15 }}
+            title="Start Here"
+            color="#46aeff"
+          />
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
+
+const styles= StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor:COLORS.white,
+  }
+})
 
 export default Onboarding;
